98dd4cdd761fda777d446d00d8a575f88925e49e
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-12 22:37:08 -0700

    Add BlockPaperScissors contract to migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
 var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
 var Authentication = artifacts.require("./Authentication.sol");
+var BlockPaperScissors = artifacts.require("./BlockPaperScissors.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
   deployer.link(Ownable, Killable);
   deployer.deploy(Killable);
   deployer.link(Killable, Authentication);
-  deployer.deploy(Authentication);
+  deployer.deploy(BlockPaperScissors);
 };
