a6a3d9d78efd6a79d12c6d24ceb15f8d38b790b0
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-14 19:33:28 -0700

    Add player messaging actions

--- src/game/ui/players/PlayerActions.js
+++ src/game/ui/players/PlayerActions.js
@@ -1,8 +1,72 @@
 export const UPDATE_PLAYERS = 'UPDATE_PLAYERS'
+export const ON_RECEIVE_INVITE = 'ON_RECEIVE_INVITE'
+export const ON_RECEIVE_CANCEL_INVITE = 'ON_RECEIVE_CANCEL_INVITE'
+export const ON_RECEIVE_ACCEPT_INVITE = 'ON_RECEIVE_ACCEPT_INVITE'
+export const ON_RECEIVE_REJECT_INVITE = 'ON_RECEIVE_REJECT_INVITE'
+export const SEND_INVITE = 'SEND_INVITE'
+export const CANCEL_INVITE = 'CANCEL_INVITE'
+export const ACCEPT_INVITE = 'ACCEPT_INVITE'
+export const REJECT_INVITE = 'REJECT_INVITE'
 
 export function updatePlayers(players) {
   return {
     type: UPDATE_PLAYERS,
     players: players
+  }
+}
+
+export function onReceiveInvite(sender) {
+  return {
+    type: ON_RECEIVE_INVITE,
+    sender
+  }
+}
+
+export function onReceiveCancelInvite(sender) {
+  return {
+    type: ON_RECEIVE_CANCEL_INVITE,
+    sender
+  }
+}
+
+export function onReceiveAcceptInvite(sender) {
+  return {
+    type: ON_RECEIVE_ACCEPT_INVITE,
+    sender
+  }
+}
+
+export function onReceiveRejectInvite(sender) {
+  return {
+    type: ON_RECEIVE_REJECT_INVITE,
+    sender
+  }
+}
+
+export function sendInvite(recipient) {
+  return {
+    type: SEND_INVITE,
+    recipient
+  }
+}
+
+export function cancelInvite(recipient) {
+  return {
+    type: CANCEL_INVITE,
+    recipient
+  }
+}
+
+export function acceptInvite(recipient) {
+  return {
+    type: ACCEPT_INVITE,
+    recipient
+  }
+}
+
+export function rejectInvite(recipient) {
+  return {
+    type: REJECT_INVITE,
+    recipient
   }
 }
