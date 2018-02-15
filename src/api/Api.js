fa05f3e45eec616eff8b6afd5e55c082555e025d
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-14 19:32:53 -0700

    Add message forwarding support to API interface

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,20 @@
 import openSocket from 'socket.io-client';
 const  socket = openSocket('http://localhost:8080');
 
+export const INVITE_MESSAGE = 'invite'
+export const CANCEL_INVITE_MESSAGE = 'cancelInvite'
+export const ACCEPT_INVITE_MESSAGE = 'acceptInvite'
+export const REJECT_INVITE_MESSAGE = 'rejectInvite'
+
 export function subscribeToAddresses(myAddress, cb) {
   socket.on('addresses', cb);
   socket.emit('introduction', { address: myAddress })
+}
+
+export function subscribeToMessages(cb) {
+  socket.on('receiveMessage', cb);
+}
+
+export function sendMessage({recipient, message, meta}) {
+  socket.emit('sendMessage', { recipient, message, meta })
 }
