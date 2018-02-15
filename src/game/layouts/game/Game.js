10b0e8f3c45b82aaf61285123cd4a9ad05e5e59e
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-14 17:23:02 -0700

    Display players in app

--- src/game/layouts/game/Game.js
+++ src/game/layouts/game/Game.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import GameMain from '../../ui/main/GameMain'
+import Players from '../../ui/players/Players'
 
 class Game extends Component {
   render() {
     return(
       <div>
         <GameMain />
+        <Players />
       </div>
     )
   }
