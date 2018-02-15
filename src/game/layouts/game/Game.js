903516f71ad287f2416088178d335735de190a7b
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-14 17:35:08 -0700

    Display web3 connection status

--- src/game/layouts/game/Game.js
+++ src/game/layouts/game/Game.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import GameMain from '../../ui/main/GameMain'
 import Players from '../../ui/players/Players'
+import Web3Status from '../../ui/web3Status/Web3Status'
 
 class Game extends Component {
   render() {
@@ -8,6 +9,7 @@ class Game extends Component {
       <div>
         <GameMain />
         <Players />
+        <Web3Status />
       </div>
     )
   }
---
+++ src/game/ui/web3Status/Web3Status.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => {
+  return {
+    web3Instance: state.web3.web3Instance
+  }
+}
+
+let Web3Status = ({ web3Instance }) => {
+  let hasAddress = web3Instance && web3Instance.eth.accounts[0]
+  let status = hasAddress ? 'Connected to web3 :)' : 'Not connected to web3 :('
+  return(
+    <div className="web3-status">
+      { status }
+    </div>
+  )
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Web3Status)
