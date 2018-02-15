753e5c9d4f602faa81a6854abd14d0306d3f67ab
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-15 01:50:52 -0700

    Post plays to blockchain

--- src/game/ui/choiceButton/ChoiceButton.js
+++ src/game/ui/choiceButton/ChoiceButton.js
@@ -1,24 +1,52 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { play } from './ChoiceButtonActions'
+import store from '../../../store'
+import BlockPaperScissorsContract from '../../../../build/contracts/BlockPaperScissors.json'
+
+const contract = require('truffle-contract')
+
+const postPlayToBlockchain = (opponent, choice, gameId, cb) => {
+  let web3 = store.getState().web3.web3Instance
+
+  const blockPaperScissors = contract(BlockPaperScissorsContract)
+  blockPaperScissors.setProvider(web3.currentProvider)
+
+  // Get current ethereum wallet.
+  web3.eth.getCoinbase((error, coinbase) => {
+    blockPaperScissors.deployed().then(function(blockPaperScissorsInstance) {
+      blockPaperScissorsInstance.createPlay(opponent, choice, gameId, {from: coinbase}).then(cb)
+    })
+  })
+}
+
+const encryptChoice = (choice, key) => {
+  return choice // TODO
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    choice: ownProps.choice
+    choice: ownProps.choice,
+    opponent: state.game.opponent,
+    gameId: state.game.gameId
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onChoiceClick: choice => {
-      dispatch(play(choice))
+    onChoiceClick: (opponent, choice, gameId) => {
+      let key = Date.now()
+      let encryptedChoice = encryptChoice(choice, key)
+      postPlayToBlockchain(opponent, encryptedChoice, gameId, () => {
+        dispatch(play(choice))
+      })
     }
   }
 }
 
-let ChoiceButton = ({ onChoiceClick, choice }) => {
+let ChoiceButton = ({ onChoiceClick, opponent, choice, gameId }) => {
   return (
-    <button onClick={() => onChoiceClick(choice)}>{choice}</button>
+    <button onClick={() => onChoiceClick(opponent, choice, gameId)}>{choice}</button>
   )
 }
 
@@ -28,4 +56,3 @@ const ChoiceButtonContainer = connect(
 )(ChoiceButton)
 
 export default ChoiceButtonContainer
-
