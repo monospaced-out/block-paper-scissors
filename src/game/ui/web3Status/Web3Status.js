dbec75d3fb8252694a91fee37f882e22d90e76dc
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-18 00:33:31 -0700

    Add link to github

--- src/game/ui/web3Status/Web3Status.js
+++ src/game/ui/web3Status/Web3Status.js
@@ -11,9 +11,14 @@ let Web3Status = ({ web3Instance }) => {
   let hasAddress = web3Instance && web3Instance.eth.accounts[0]
   let status = hasAddress ? 'You are connected to web3. :)' : 'You are not connected to web3. :('
   return(
-    <p className="web3-status">
-      This is currently running on the Rinkeby testnet. { status }
-    </p>
+    <div className="footer">
+      <p>
+        This is currently running on the Rinkeby testnet. { status }
+      </p>
+      <p>
+        <a href="https://github.com/monospaced-out/block-paper-scissors" target="_blank">github.com/monospaced-out/block-paper-scissors</a>
+      </p>
+    </div>
   )
 }
 
